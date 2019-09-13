c164af65badcb05a5781aca74fa840ae96ed46b4
Ameer Dawood <ameer1234567890@gmail.com>
2019-09-13 22:17:46 +0500

    Fix ThingSpeak ping

--- router.setup.sh
+++ router.setup.sh
@@ -1163,7 +1163,7 @@ setup_thingspeak_ping() {
     else
       crontab -l > crontab.txt 2>/dev/null
       status_list="$?"
-      echo "* * * * * curl \"https://api.thingspeak.com/update?api_key=9QY15FLFX4REDCQ5&field1=$(grep MemFree /proc/meminfo | awk '{print $2}')\"" >> crontab.txt
+      echo "* * * * * curl \"https://api.thingspeak.com/update?api_key=9QY15FLFX4REDCQ5&field1=\$(grep MemFree /proc/meminfo | awk '{print \$2}')\"" >> crontab.txt
       status_one="$?"
       crontab crontab.txt >/dev/null 2>&1
       status_install="$?"
