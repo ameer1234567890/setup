4587fd665ad909c779fc15daa87b73ae73331f51
Ameer Dawood <ameer1234567890@gmail.com>
2019-09-20 21:54:01 +0500

    Fix progress indicator v2

--- router.setup.sh
+++ router.setup.sh
@@ -128,7 +128,7 @@ show_progress() {
       7 ) printf "  ⠇\b\b\b" ;;
       8 ) printf "  ⠏\b\b\b" ;;
     esac
-    progress_state=$((progress_state + 1))
+    progress_state=$(( progress_state + 1 ))
     if [ $progress_state -gt 8 ]; then
       progress_state=0
     fi
