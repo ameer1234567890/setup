9a8be2637e00865f584e955716aaee9ea41bea6a
Ameer Dawood <ameer1234567890@gmail.com>
2024-11-18 16:56:25 +0500

    Fixed cups ssl setup

--- pi.setup.sh
+++ pi.setup.sh
@@ -1101,7 +1101,7 @@ setup_cups_ssl() {
   else
     rm /etc/cups/ssl/printer.lan.crt 2>/dev/null && \
       ln -s /mnt/$USB_DATA_DEVICE/docker/tls/printer.crt /etc/cups/ssl/printer.lan.crt && \
-      rm /etc/cups/ssl/printer.lan.key && \
+      rm /etc/cups/ssl/printer.lan.key 2>/dev/null && \
       ln -s /mnt/$USB_DATA_DEVICE/docker/tls/printer.key /etc/cups/ssl/printer.lan.key && \
       systemctl restart cups.service &
     bg_pid=$!
